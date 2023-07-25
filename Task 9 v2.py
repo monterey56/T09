9334f99c08b1cae7e1d695e13a20b1e230ee16e0
monterey56 <wowpunkwar@gmail.com>
2023-07-25 12:49:58 +0100

    overhaul
    Old code inefficient and didn't work as intended. Rewritten from ground up to address issues

--- Task 9 v2.py
+++ Task 9 v2.py
@@ -1,28 +1,26 @@
 import os
 
 def write_to_file(content):
-    with open("calculations.txt", 'x') as f:
+    with open("calculations.txt", 'a') as f:
         f.write(content + "\n")
 
-#operations and zero division error
-def perform_operation(x, y, operation):
+def perform_operation(a, b, operation):
     if operation == '+':
-        return x + y
+        return a + b
     elif operation == '-':
-        return x - y
+        return a - b
     elif operation == 'x':
-        return x * y
+        return a * b
     elif operation == '/':
-        if y != 0:
-            return x / y
+        if b != 0:
+            return a / b
         else:
             print("Cannot divide by zero.")
             return None
     else:
         print("Unsupported operation.")
         return None
-    
-#read from txt file
+
 def read_from_file(filename):
     if not os.path.exists(filename):
         print(f"File {filename} does not exist.")
@@ -31,28 +29,26 @@ def read_from_file(filename):
     with open(filename, 'r') as f:
         lines = f.readlines()
 
-#expected format for equations to be read
     for line in lines:
-        x, operation, y = line.split()
-        x, y = int(x), int(y)
-        result = perform_operation(x, y, operation)
+        a, operation, b = line.split()
+        a, b = int(a), int(b)
+        result = perform_operation(a, b, operation)
         if result is not None:
-            print(f"{x} {operation} {y} = {result}")
+            print(f"{a} {operation} {b} = {result}")
 
-#take user inputs and store as variables
 def calculator():
     while True:
         mode = input("Enter '1' to perform calculation, '2' to read from file, '3' to exit: ")
 
         if mode == '1':
-            x = int(input("Enter first number: "))
-            y = int(input("Enter second number: "))
+            a = int(input("Enter first number: "))
+            b = int(input("Enter second number: "))
             operation = input("Enter operation (+, -, x, /): ")
 
-            result = perform_operation(x, y, operation)
+            result = perform_operation(a, b, operation)
             if result is not None:
-                print(f"{x} {operation} {y} = {result}")
-                write_to_file(f"{x} {operation} {y} = {result}")
+                print(f"{a} {operation} {b} = {result}")
+                write_to_file(f"{a} {operation} {b} = {result}")
         elif mode == '2':
             filename = input("Enter filename: ")
             read_from_file(filename)
